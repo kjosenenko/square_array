89dbb01f6e52fd68c1048b7ac1afc212e9724a6f
KJ Osenenko <karljosenenko@gmail.com>
2020-11-10 15:08:02 -0700

    commit tree 952e0eac9b0b1540756e2ba5e31a4e32d0f85b09

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  squared_numbers = []
+  array.each do |number|
+    squared_numbers.push(number * number)
+  end
+  puts squared_numbers
+  return squared_numbers
 end
+
+# square_array([1, 2 , 3])
